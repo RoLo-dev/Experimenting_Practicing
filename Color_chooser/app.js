b43168823d5dda9bc1f68f390e5e76098836aa8b
Rolando Lopantzi <rlopantzi@gmail.com>
2020-05-30 12:15:53 -0700

    init commit

--- Color_chooser/app.js
+++ Color_chooser/app.js
@@ -1,13 +1,13 @@
 // This is the function how you would generate random hex colors
-function randomHex() {
-    const letters = "0123456789ABCDEF";
-    let hash = "#";
-    for(let i = 0; i < 6; i++) {
-        hash += letters[Math.floor(Math.random() * 16)];
-    }
-    return hash;
-}
-console.log(randomHex());
+// function randomHex() {
+//     const letters = "0123456789ABCDEF";
+//     let hash = "#";
+//     for(let i = 0; i < 6; i++) {
+//         hash += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return hash;
+// }
+// console.log(randomHex());
 
 const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelectorAll('.generate');
@@ -16,6 +16,11 @@ const currentHexes = document.querySelectorAll(".color h2");
 let initialColors;
 
 // This takes care of generating colors
+function randomHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
 function randomColors() {
     colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
@@ -24,6 +29,35 @@ function randomColors() {
         // Applying the generated hex as the background color
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        // Checking for contrast
+        checkTextContrast(randomColor, hexText);
+        // Initialize color sliders
+        const color =  chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders(color, hue, brightness, saturation);
     });
+}
+
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if(luminance > 0.5) {
+        text.style.color = "black";
+    } else {
+        text.style.color = "white";
+    }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    // Scale saturation
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+    // Update input colors
+    saturation.style.backgroundImage = `linear-gradient(to-right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
 }
 randomColors();
