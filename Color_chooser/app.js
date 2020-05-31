a8edf8375659b94a2f4598413ae1c0f73c94d8d3
Rolando Lopantzi <rlopantzi@gmail.com>
2020-05-30 20:46:02 -0700

    Update app.js

--- Color_chooser/app.js
+++ Color_chooser/app.js
@@ -25,6 +25,11 @@ function mouseHover(controls) {
     }
 }
 
+// Event listeners
+sliders.forEach(slider => {
+    slider.addEventListener('input', hslControls);
+})
+
 // This takes care of generating colors
 function randomHex() {
     const hexColor = chroma.random();
@@ -77,4 +82,14 @@ function colorizeSliders(color, hue, brightness, saturation) {
 
     hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
+
+function hslControls(e) {
+    const index = 
+    e.target.getAttribute('data-bright') ||
+    e.target.getAttribute('data-sat') || 
+    e.target.getAttribute('data-hue');
+
+    let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+}
+
 randomColors();
