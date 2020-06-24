e5aa6b3dea8932fa74a072aeaa6b53da2fef0963
Rolando Lopantzi <rlopantzi@gmail.com>
2020-06-24 09:54:26 -0700

    init commit

--- portfolio_react/src/MobileBanner.js
+++ portfolio_react/src/MobileBanner.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 class MobileBanner extends Component {
     render() {
+        const logo = require('./img/RL_favicon-2018.png');
         return(
             <header className="mobile-header">
                 <div className="wrapper">
                     <div className="title">
-                        <h1>R</h1>
+                        <img src={logo} alt=""/>
                     </div>
                     <div id="hamburger-menu">
-                        
+                        <span className="open-icon"></span>
                     </div>
                 </div>
             </header>
