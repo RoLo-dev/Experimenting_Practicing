bb60ddffd49eaab7a94b6bb1fc9718bd86887969
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-19 15:59:12 -0700

    init commit

--- dice-app/src/Die.js
+++ dice-app/src/Die.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Die extends Component {
     render() {
-        return <i class="fas fa-dice-two"></i>
+        return <i className={'fas fa-dice-${this.props.face}'}></i>
     }
 }
 
