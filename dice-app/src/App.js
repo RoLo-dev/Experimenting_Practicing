f6a313f4ddcfffa1ea783599477a7fa2edb75897
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-19 16:31:37 -0700

    init commit

--- dice-app/src/App.js
+++ dice-app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Die from './Die';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Die face='five' />
     </div>
   );
 }
