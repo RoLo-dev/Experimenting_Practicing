99f8cade20feb72a5c4e8c1c8b55cdd8ee301bf5
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-20 14:38:56 -0700

    Update RollDice.js

--- dice-app/src/RollDice.js
+++ dice-app/src/RollDice.js
@@ -8,6 +8,12 @@ class RollDice extends Component {
     constructor(props) {
         super(props);
         this.state = { die1: "one", die2: "one" };
+        this.roll = this.roll.bind(this);
+    }
+    roll() {
+        const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+        this.setState({ die1: newDie1, die2: newDie2 });
     }
     render() {
         return (
