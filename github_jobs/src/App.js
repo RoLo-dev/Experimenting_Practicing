061a3926401123a4f779625bebdd2c2f75716726
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-21 15:37:31 -0700

    init commit

--- github_jobs/src/App.js
+++ github_jobs/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import FetchJobs from './FetchJobs';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  const { jobs, loading, error } = FetchJobs()
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error, try refreshing</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
