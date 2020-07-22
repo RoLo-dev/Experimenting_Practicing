d65c2cd2246fd056fd613505583cd357da575b89
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-21 21:24:26 -0700

    init commit

--- github_jobs/src/FetchJobs.js
+++ github_jobs/src/FetchJobs.js
@@ -1,13 +1,40 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
 
-function reducer(state, action ) {}
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
+}
 
-export default function fetchJobs(params, page) {
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+function reducer(state, action ) {
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: [] }
+        default:
+            return state
+    }
+}
+
+export default function FetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
-    return {
-        jobs: [],
-        loading: false,
-        error: false
-    }
+    useEffect(() => {
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            params: { markdown: true, page: page, ...params }
+        }).then(res => {
+            dispatch({ type:ACTIONS.GET_DATA, payload: { jobs: res.data } })
+        }).catch(e => {
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+        })
+    }, [params, page])
+
+    return state
 }
