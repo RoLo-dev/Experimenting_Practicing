0b279807391ccf23b05dc9cd66a82a31f0420613
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-21 20:27:05 -0700

    Update FetchJobs.js

--- github_jobs/src/FetchJobs.js
+++ github_jobs/src/FetchJobs.js
@@ -1,4 +1,10 @@
+import { useReducer } from 'react'
+
+function reducer(state, action ) {}
+
 export default function fetchJobs(params, page) {
+    const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+
     return {
         jobs: [],
         loading: false,
