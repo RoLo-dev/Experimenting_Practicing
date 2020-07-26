e26707728e58a8d44539f053d9d694e12427d3b0
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-26 10:42:04 -0700

    Update App.js

--- basic-site-react/src/App.js
+++ basic-site-react/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.scss';
 
 class App extends React.Component {
@@ -27,6 +28,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <Router></Router>
             </div>
         )
     }
