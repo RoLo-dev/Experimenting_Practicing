a3b06bc6abb176d8f84d9f545db58a0a26823d68
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-26 09:27:08 -0700

    init commit

--- basic-site-react/src/App.js
+++ basic-site-react/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Rolando',
+            headerLinks: [
+                { title: 'Home', path: '/' },
+                { title: 'About', path: '/about' },
+                { title: 'Contact', path: '/contact' }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+            </div>
+        )
+    }
 }
 
 export default App;
