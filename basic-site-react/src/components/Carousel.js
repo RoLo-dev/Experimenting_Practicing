355c9a7404b3cbe715f06b35546fd01c707c1716
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-28 21:12:12 -0700

    init commit

--- basic-site-react/src/components/Carousel.js
+++ basic-site-react/src/components/Carousel.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import Img1 from '../assets/img/img1.jpg';
+import Img2 from '../assets/img/img2.jpg';
+import Img3 from '../assets/img/img3.jpg';
+
 class Carousel extends React.Component {
 
     constructor(props) {
@@ -10,7 +14,23 @@ class Carousel extends React.Component {
                     id: 0,
                     title: 'Project 1',
                     subTitle: 'The first card',
-                    imgSrc: '',
+                    imgSrc: Img1,
+                    link: 'https://unsplash.com/',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'Project 2',
+                    subTitle: 'The second card',
+                    imgSrc: Img2,
+                    link: 'https://unsplash.com/',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'Project 3',
+                    subTitle: 'The third card',
+                    imgSrc: Img3,
                     link: 'https://unsplash.com/',
                     selected: false
                 }
