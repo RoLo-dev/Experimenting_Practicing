751a83af9302cafbd895e60a53e5ca21cd5fa44f
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-29 10:11:07 -0700

    init commit

--- basic-site-react/src/components/CardInfo.js
+++ basic-site-react/src/components/CardInfo.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
 
 function CardInfo(props) {
+    const style = useSpring({opacity: 1, from: {opacity: 0}})
+
     return(
-        <p>CardInfo works</p>
+        <animated.div className="" style={style}>
+            <p>{props.title}</p>
+            <p>{props.subTitle}</p>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+        </animated.div>
     )
 }
 
