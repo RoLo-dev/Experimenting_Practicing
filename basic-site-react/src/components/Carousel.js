c9e8d9730b69757aac26a92efdc9aad22a821ae4
Rolando Lopantzi <rlopantzi@gmail.com>
2020-07-30 12:08:05 -0700

    init commit

--- basic-site-react/src/components/Carousel.js
+++ basic-site-react/src/components/Carousel.js
@@ -53,7 +53,7 @@ class Carousel extends React.Component {
     }
     makeItems = (items) => {
         return items.map(item => {
-            return <Card item={item} onClick={(e => this.cardClick(item.id, e))} key={item.id} />
+            return <Card item={item} click={(e => this.cardClick(item.id, e))} key={item.id} />
         })
     }
 
