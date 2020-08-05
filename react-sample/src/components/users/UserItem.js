6de7647b47d020bfe15b29fe4373d43ab78b8611
Rolando Lopantzi <rlopantzi@gmail.com>
2020-08-05 11:46:02 -0700

    init commit

---
+++ react-sample/src/components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello from UserItem</p>
+            </div>
+        )
+    }
+}
+
+export default UserItem
