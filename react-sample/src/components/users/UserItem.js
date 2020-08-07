9164d7b86b3bec65f9a56dde66ebea23e7ae9e87
Rolando Lopantzi <rlopantzi@gmail.com>
2020-08-07 16:51:02 -0700

    init commit

--- react-sample/src/components/users/UserItem.js
+++ react-sample/src/components/users/UserItem.js
@@ -1,31 +1,19 @@
 // Shortcut to is "rce" to write the import React and 
 // export class and export default
-import React, { Component } from 'react'
+import React from 'react'
 
-export class UserItem extends Component {
-    constructor() {
-        super();
-        this.state = {
-            id: "1",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo"
-        }
-    }
+const UserItem = (props) => {
+    // you can clean up your code by having all the state
+    // properties as a variable and have it equal to this.state
+    const { avatar_url, login, html_url } = props.user;
 
-    render() {
-        // you can clean up your code by having all the state
-        // properties as a variable and have it equal to this.state
-        const { avatar_url, login, html_url } = this.state;
-
-        return <div className="card text-center">
-            <img src={avatar_url} alt="" className="round-img" style= {{ width: '60px' }}/>
-            <h3>{login}</h3>
-            <div>
-                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-            </div>
+    return <div className="card text-center">
+        <img src={avatar_url} alt="" className="round-img" style= {{ width: '60px' }}/>
+        <h3>{login}</h3>
+        <div>
+            <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
-    }
+    </div>
 }
 
 export default UserItem
