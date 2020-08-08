a6f124a98918e960415a65194e935b5facd36c23
Rolando Lopantzi <rlopantzi@gmail.com>
2020-08-08 13:07:23 -0700

    init commit

--- react-sample/src/components/users/UserItem.js
+++ react-sample/src/components/users/UserItem.js
@@ -1,6 +1,7 @@
 // Shortcut to is "rce" to write the import React and 
 // export class and export default
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserItem = ({ user: { avatar_url, login, html_url } }) => {
     // you can clean up your code by having all the state
@@ -14,6 +15,9 @@ const UserItem = ({ user: { avatar_url, login, html_url } }) => {
             <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
     </div>
+}
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
