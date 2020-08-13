95afb559301a23037502b6a34319362cd068dab6
Rolando Lopantzi <rlopantzi@gmail.com>
2020-08-12 17:14:25 -0700

    init commit

--- portfolio_react/src/App.js
+++ portfolio_react/src/App.js
@@ -3,14 +3,14 @@ import './App.scss';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import Banner from './components/Banner.js';
-// import SideMenu from './components/SideMenu.js'
+import SideMenu from './components/SideMenu.js'
 
 function App() {
     return(
         <Router>
             <div>
                 <Banner />
-                {/* <SideMenu /> */}
+                <SideMenu />
             </div>
         </Router>
     )
