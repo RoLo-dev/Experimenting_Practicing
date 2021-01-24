6512afd33bdefb2a111d7dab3cbce93c1b6fd07d
Rolando Lopantzi <rlopantzi@gmail.com>
2021-01-23 21:52:58 -0800

    init commit

--- Vue/modal-project/src/App.vue
+++ Vue/modal-project/src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <Modal header="Sign up for Giveaway" text="Grab our swag that will only be available for a limited time only" />
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" @close="toggleModal"/>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
@@ -10,6 +13,19 @@ export default {
   name: 'App',
   components: {
     Modal
+  },
+  data() {
+    return {
+      title: "Working with Vue",
+      header: "Sign up for the Giveaway",
+      text: "Grab our swag that will only be available for a limited time only",
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
--- Vue/modal-project/src/components/Modal.vue
+++ Vue/modal-project/src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="backdrop">
+    <div class="backdrop" @click.self="closeModal">
         <div class="modal">
             <h1>{{ header }}</h1>
             <p>{{ text }}</p>
@@ -9,7 +9,12 @@
 
 <script>
 export default {
-    props: [ "header", "text" ]
+    props: [ "header", "text" ],
+    methods: {
+        closeModal() {
+            this.$emit("close")
+        }
+    }
 }
 </script>
 
