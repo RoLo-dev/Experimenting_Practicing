a14a88b9ba31ffd62da9ceaf1a2098c38eef9e4b
Rolando Lopantzi <rlopantzi@gmail.com>
2021-02-20 22:18:02 -0800

    init commit

--- StoreLocator/script.js
+++ StoreLocator/script.js
@@ -1,9 +1,18 @@
 const desktopFilterBtn = document.getElementById("desktop-filter");
 const filterItems = document.getElementById("filter-items");
+const mobileFilterIcon = document.getElementById("mobile-filter");
 
 desktopFilterBtn.addEventListener("click", showFilter);
+mobileFilterIcon.addEventListener("click", showFilter);
 
 function showFilter() {
-    console.log("clicked");
     filterItems.classList.toggle("show");
 }
+
+let mobileMenuBtns = document.getElementsByClassName("mobile-menu-btns");
+
+mobileMenuBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("active");
+    })
+});
