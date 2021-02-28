dc473cc225245bf8b362559b602dbfd494d28197
Rolando Lopantzi <rlopantzi@gmail.com>
2021-02-28 15:47:24 -0800

    init commit

--- Kinectiv/script.js
+++ Kinectiv/script.js
@@ -1,2 +1,14 @@
 let monthBtn = document.getElementById("monthly-btn");
 let yearBtn = document.getElementById("yearly-btn");
+
+yearBtn.addEventListener("click", showYearPrice);
+monthBtn.addEventListener("click", showMonthPrice);
+
+function showYearPrice() {
+    yearBtn.classList.add("active-year");
+    monthBtn.classList.add("inactive-month");
+}
+function showMonthPrice() {
+    monthBtn.classList.remove("inactive-month");
+    yearBtn.classList.remove("active-year");
+}
