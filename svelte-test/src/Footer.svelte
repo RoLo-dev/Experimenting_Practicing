140b0fec6ce1363f3f3de7edfab25b5b182287a0
Rolando Lopantzi <167485096+rolando-lopantzi@users.noreply.github.com>
2024-08-27 22:56:21 +0700

    adding footer

---
+++ svelte-test/src/Footer.svelte
@@ -0,0 +1,92 @@
+<footer>
+    <div class="wrapper ftContent">
+        <div class="ftLogo">
+            <a href="index.html"><img loading="lazy" src="https://banibakery.com/images/bani_logo_17.png" alt="Bani bakery logo" class="pics"></a>
+            <p class="copyRight">&copy; <span id="footer-year"></span> banibakery.com</p>
+        </div>
+        <div class="ftLinks">
+            <h4>Bani Bakery</h4>
+            <ul>
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="index.html#sweetTable" class="navLink">Sweet Table</a></li>
+                <li><a href="index.html#flavorFill" class="navLink">Flavors & Fillings</a></li>
+                <li><a href="index.html#about" class="navLink">About</a></li>
+                <li><a href="index.html#contact">Contact</a></li>
+                <li><a href="careers.html">Careers</a></li>
+                <li><a href="reviews.html">Reviews</a></li>
+                <li><a href="terms-conditions.html">Terms & Conditions</a></li>
+            </ul>
+            <h4 class="social">Social</h4>
+            <ul class="socialList">
+                <li>
+                    <a href="https://www.facebook.com/banibakery/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/banihouseofcakes/" target="_blank"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.yelp.com/biz/bani-bakery-santa-ana" target="_blank"><i class="fab fa-yelp"></i></a>
+                </li>
+            </ul>
+        </div>
+        <!-- <div class="award">
+            <div>
+                <img src="images/best.png" class="pics" alt="Award for best business of 2018">
+            </div>
+            <div class="background-white">
+                <img loading="lazy" src="https://threebestrated.com/awards/cakes-santa_ana-2019-clr.svg" alt="Best Cakes in Santa Ana" class="pics">
+            </div>
+        </div> -->
+        <div class="business-info">
+            <div class="storehrs">
+                <h4>Store Hours</h4>
+                <ul>
+                    <li>Mon - CLOSED</li>
+                    <li>Tue - CLOSED</li>
+                    <li>Wed - Fri: 10am - 6pm</li>
+                    <li>Sat - Sun: 9am - 4pm</li>
+                </ul>
+            </div>
+            <div class="business-contact">
+                <h4>Contact</h4>
+                <ul>
+                    <li>
+                        <a href="mailto:banibakery@gmail.com"> banibakery@gmail.com</a>
+                    </li>
+                    <li>
+                        <a href="tel:7142495911">(714) 249 - 5911</a>
+                    </li>
+                    <li>
+                        <a href="https://goo.gl/maps/XsePsz1xZJ2xge2i8" target="_blank">7561 Center Ave #7, Huntington Beach, CA 92647</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer{ background-color: #814d3b;height: auto; }
+    .ftContent{ display: flex;justify-content: space-between;padding: 2.5rem 0 2rem 0; }
+    .ftLogo{ display: flex;flex-direction: column;width: 20%; }
+    .copyRight{ color: #FFF;text-align: center;margin-top: 1rem;text-decoration: none; }
+    .business-info{ color: #FFF;width: 15%; }
+    .business-info .storehrs{ margin-bottom: 1rem; }
+    .business-info h4{ margin-bottom: 12px; }
+    .business-info ul li{ margin-bottom: 7px; }
+    .business-info ul li a{ color: #FFF;text-decoration: none; }
+    .award{ width: 11%; }
+    .award .background-white{
+        background-color: #FFF;
+        border-radius: 130px;
+        padding: 15px;
+        margin-top: 10px;
+    }
+    .ftLinks h4{ color: #FFF;margin-bottom: 12px; }
+    .ftLinks .social{ margin-top: 1rem; }
+    .ftLinks .socialList li{ display: inline-block;margin-right: 10px;font-size: 1.35rem; }
+    .ftLinks ul li{ list-style: none;margin-bottom: 7px; }
+    .business-info ul li:last-child, .ftLinks ul li:last-child{ margin-bottom: 0; }
+    .ftLinks ul li a{ text-decoration: none;color: #FFF;transition: all 0.9s; }
+    .ftLinks ul li a:hover{ color: #F7ACB8;transition: 0.3s ease-in; }
+</style>
